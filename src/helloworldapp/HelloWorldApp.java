f0745698965b3322706406a63f72bdbcf4e6c42d
brian <brian@XPS15>
2018-01-24 11:47:21 +0000

    Initial Commit

---
+++ src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldapp;
+
+/**
+ *
+ * @author brian
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tester t;
+        t = new Tester();
+        t.doSomething();
+        
+        
+        // TODO code application logic here
+    }
+
+}
---
+++ src/helloworldapp/Tester.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldapp;
+
+/**
+ *
+ * @author brian
+ */
+public class Tester {
+
+    public Tester() {
+    }
+    public void doSomething()
+    {
+        System.out.println("Tester.doSomething()");
+    }
+}
